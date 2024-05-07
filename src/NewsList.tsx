84d89f738aa300b60f133673f075b0d38ee7d159
Koketso Morapedi <53305777+Koketso1999@users.noreply.github.com>
2024-05-07 12:57:13 +0200

    Fix: Typescript QA Errors

--- src/NewsList.tsx
+++ src/NewsList.tsx
@@ -2,15 +2,11 @@ import React from "react";
 import { useQuery } from "react-query";
 import NewsCard from "./NewsCard";
 import { Skeleton } from "./components/ui/skeleton";
-import { Alert, AlertDescription, AlertTitle } from "./components/ui/alert";
-import { Terminal } from "lucide-react";
 import { ToastAction } from "@/components/ui/toast";
 import { useToast } from "@/components/ui/use-toast";
 
-
-
 const NewsList: React.FC = () => {
-    const { toast } = useToast();
+  const { toast } = useToast();
   const { isLoading, error, data } = useQuery("news", async () => {
     const response = await fetch(
       "https://newsapi.org/v2/everything?q=apple&from=2024-05-04&to=2024-05-04&sortBy=popularity&apiKey=c10fad271ac04705af6f553aa6031dc8"
@@ -29,30 +25,20 @@ const NewsList: React.FC = () => {
       </div>
     );
   if (error)
-    return () => {
-      toast({
-        variant: "destructive",
-        title: "Uh oh! Something went wrong.",
-        description: "There was an error fetching the news.",
-        action: <ToastAction altText="Try again">Try again</ToastAction>,
-      });
-    };
-  
-  
-  
-  
-  // (
-  //     <Alert variant="destructive">
-  //       <Terminal className="h-4 w-4" />
-  //       <AlertTitle>Heads up!</AlertTitle>
-  //       <AlertDescription>Error: {(error as Error).message}</AlertDescription>
-  //     </Alert>
-  //   );
+    return (
+      <>
+        {toast({
+          variant: "destructive",
+          title: "Uh oh! Something went wrong.",
+          description: "There was an error fetching the news.",
+          action: <ToastAction altText="Try again">Try again</ToastAction>,
+        })}
+      </>
+    );
 
   console.log(data);
 
   return (
-    //
     <div className="news-list p-10">
       {data.articles.map((article: any) => (
         <NewsCard
